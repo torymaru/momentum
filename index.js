ba9b7737daded40011e9d42efa2a234780e3666f
torymaru <109635727+torymaru@users.noreply.github.com>
2022-11-07 15:02:37 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    renderCurrentTime();
+    renderQuote();
+});
+
+//real time
+function renderCurrentTime() {
+let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
+fetch(url)
+.then(res => res.json()).then((data) => {
+    let datetime = data['datetime'].substr(11,5);
+    $('#time').text(datetime);
+});
+}
+
+//Famous saying
+function renderQuote() {
+    let url = `https://api.quotable.io/random`;
+    fetch(url)
+        .then(res => res.json()).then((data) => {
+            let content = `" ${data['content']} "`;
+            let author = `- ${data['author']} -`;
+            $('#content').text(content);
+            $('#author').text(author);
+        });
+}
